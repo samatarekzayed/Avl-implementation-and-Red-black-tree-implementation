3e45bff898c801994a1ab0e5eeebf2595be2a1e2
mariamelsamni <96484561+mariamelsamni@users.noreply.github.com>
2023-03-17 19:51:11 +0200

    first attempt delete added

--- Avl and RedBlack  trees implementations/src/avl.java
+++ Avl and RedBlack  trees implementations/src/avl.java
@@ -1,3 +1,4 @@
+import static sun.swing.MenuItemLayoutHelper.max;
 
 class avl {
     avlnode root = null;
@@ -7,12 +8,13 @@ class avl {
         this.root = null;
     }
     //////////////////////////////////////////////////////////
-    boolean isBalanced(avlnode u) {
-//        if (u == null)
-//            return false;
-        return Math.abs(height(u.left) - height(u.right)) >= 2;
-    }
 
+    int balanced(avlnode u) {
+        if (u == null)
+            return 0;
+
+        return u.left.height - u.right.height;
+    }
     ////////////////////////////////////////////////////////////
     avlnode search(avlnode root, int key)
     {
@@ -86,15 +88,102 @@ class avl {
         return root;
     }
 
+    avlnode inorderSucessor(avlnode n){
 
-    ///////////////////////////////////////////////////////////////
+        n=n.right; //e7na mot2kdenn enha internal w 3ndha left w right
+        while (n.left!=null){
+            n=n.left;
+        }
+        return n;
+    }
     avlnode delete(avlnode root, int key){
+        if (root==null){
+            //mbda2eyn 3shan a-handle lw elnode mesh mwgoda asln
+            return root;
+        }
+        if (key< root.key){
+            root.left=delete(root.left,key);
+        }
+        else if (key> root.key){
+            root.right=delete(root.right,key);
+        }
+        else{
+            //kda e7na la2ena el node w 3ndena 3 cases
+            //elnode leaf
+            //elnode 3ndha ebn wa7ed
+            //elnode internal
+            if ((root.left == null) && (root.right == null)){
+                root=null;
+            }
+            else if(root.left == null){
+                root=root.right;
+            }
+            else if(root.right == null){
+                root=root.left;
+            }
+            else {
+                //internallllllllllllllllllllllllllllllllllllllll
+                avlnode temp = inorderSucessor(root.right);
+                root.key=temp.key;
+                //ka2en m7slsh hga bkml 3dy 3ndy shakkk fel 4 stor ely t7t dol
+                if (key< root.key){
+                    root.left=delete(root.left,key);
+                }
+                else if (key> root.key){
+                    root.right=delete(root.right,key);
+                }
 
 
-//hhhhh
+            }
+
+        }
+        //after returning from the recursive call
+        //7n-update el height
+        root.height=max(root.left.height,root.right.height)+1;
+        //n-check lw 7sal moshkla flbalance bt3 elnode
+        int diffLR=balanced(root);
+        if (diffLR>1){
+            //eln7ya el left atwal mshet left el awal
+            if (key<root.left.key){
+
+                return rightRotate(root);
+                //case left left
+            }
+            else {
+                root.left=leftRotate(root.left);
+                return(rightRotate(root));
+                //case left right
+            }
+        }
+        else if (diffLR<-1){
+            //eln7ya elright atwal
+            if (key<root.left.key){
+                //right left
+                root.right=rightRotate(root.right);
+                return leftRotate(root);
+            }
+            else {
+                //right right
+                return leftRotate(root);
+
+            }
+
+        }
+
+        //implement deletion 3dy gedan zy el binary
+        //search 3la el key elmfrod ams7o bs mesh mstkhdmen methos search 3shan nback track w n update el heights
+        //base case
+//        if (node.height==1){
+//            node=null;
+//            //mesh 3rfa elmfrod a update kol el heights ely fl tree wla la
+//        }
+
+
 
         return root;
     }
+    ///////////////////////////////////////////////////////////////
+
     ///////////////////////////////////////////////////////////////
     int size(avlnode n)
     {
