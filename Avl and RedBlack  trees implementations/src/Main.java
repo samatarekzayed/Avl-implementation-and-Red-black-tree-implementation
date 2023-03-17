f8ca63a9ca7b7ab9cf3b8d5ed276d851b9ab5019
marshelino-maged <78962756+marshelino-maged@users.noreply.github.com>
2023-03-17 23:37:40 +0200

    remove code in the main

--- Avl and RedBlack  trees implementations/src/Main.java
+++ Avl and RedBlack  trees implementations/src/Main.java
@@ -1,18 +1,7 @@
 class Main{
     public static void main(String[] args) {
 
-        avl tree=new avl();
-        services s=new services();
-        tree.insertElement(8);
-        tree.insertElement(13);
-        tree.insertElement(15);
-        tree.insertElement(17);
-        tree.insertElement(12);
-        tree.insertElement(6);
-        tree.insertElement(7);
-        s.inorderRec(tree.root);
-        System.out.println(tree.root.left.height);
-        s.print2D(tree.root);
+        
 
 
 
