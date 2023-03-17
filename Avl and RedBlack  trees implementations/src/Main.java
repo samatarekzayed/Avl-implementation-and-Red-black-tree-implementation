b804ab68181b3672d9f4320e5dc98bd39f938b02
mariamgerges575 <96488115+mariamgerges575@users.noreply.github.com>
2023-03-17 17:52:46 +0200

    insert 1

--- Avl and RedBlack  trees implementations/src/Main.java
+++ Avl and RedBlack  trees implementations/src/Main.java
@@ -1,6 +1,16 @@
 class Main{
     public static void main(String[] args) {
 
+        avl tree=new avl();
+        tree.insertElement(8);
+        tree.insertElement(13);
+        tree.insertElement(15);
+        tree.insertElement(17);
+        tree.insertElement(12);
+        tree.insertElement(6);
+        tree.insertElement(7);
+        System.out.println(tree.root.right.right.height);
+
 
     }
 
--- Avl and RedBlack  trees implementations/src/avl.java
+++ Avl and RedBlack  trees implementations/src/avl.java
@@ -1,12 +1,16 @@
+
 class avl {
     avlnode root;
-
+    public avl()
+    {
+        this.root = null;
+    }
     //////////////////////////////////////////////////////////
     int balanced(avlnode u) {
         if (u == null)
             return 0;
 
-        return u.left.height - u.right.height;
+        return getHeight(u.left) - getHeight(u.right);
     }
 
     ////////////////////////////////////////////////////////////
@@ -38,12 +42,51 @@ class avl {
     }
 
     ///////////////////////////////////////////////////////////////
-    avlnode insert(avlnode n, int key) {
-
-
-
-      return n;
+    private int getHeight(avlnode node )
+    {
+        if(node==null)
+            return -1;
+        else
+            return node.height;
     }
+    //////////////////////////////////////////////////////////////
+    public void insertElement(int key){
+        this.root=insert(root,key);
+    }
+    private avlnode insert(avlnode root, int key) {
+        //avlnode newNode=new avlnode(key);
+        if(root!=null){
+            if (key > root.key) {
+                root.right = insert((root.right), key);
+                if (balanced(root) == -2){
+                    if(key>root.right.right.key)
+                        leftRotate(root);
+                    else {
+                        rightRotate(root.right);
+                        leftRotate(root);
+                    }
+                }
+            }
+            else {
+                root.left = insert(root.left, key);
+                if(balanced(root)==2){
+                    if(key< root.left.left.key)
+                        rightRotate(root);
+                    else{
+                        leftRotate(root.left);
+                        rightRotate(root);
+                        {
+                }
+            }
+
+        }
+        else  //reach leaf
+            root=new avlnode(key);
+
+        root.height = Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+        return root;
+    }
+
     ///////////////////////////////////////////////////////////////
     avlnode delete(avlnode root, int key){
 
