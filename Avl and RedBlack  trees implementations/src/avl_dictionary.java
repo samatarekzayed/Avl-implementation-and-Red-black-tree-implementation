7bf19e3cd5a5d2fdbd1c2c40cb8eafbbd5f9f2c5
Patrick-Geo7 <92538847+Patrick-Geo7@users.noreply.github.com>
2023-03-21 10:47:51 +0200

    Update avl_dictionary.java

--- Avl and RedBlack  trees implementations/src/avl_dictionary.java
+++ Avl and RedBlack  trees implementations/src/avl_dictionary.java
@@ -1,3 +1,6 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 public class avl_dictionary implements IDictionary{
 
 	avl dictionary = new avl<>();
@@ -43,8 +46,26 @@ public class avl_dictionary implements IDictionary{
 	 * @return
 	 */
 	@Override
-	public boolean batchInsert(String path) {
-		return false;
+	public int batchInsert(String path) {
+		int insert_count=0;
+		try{
+			
+			File myfile = new File(path);
+			Scanner myReader = new Scanner(myfile);
+			while(myReader.hasNextLine()){
+				String word = myReader.nextLine();
+				boolean inserted = dictionary.insert(word);
+				if(inserted){
+					insert_count = insert_count++;
+				}
+			}
+			myReader.close();
+		}catch (FileNotFoundException e) {
+      			System.out.println("An error occurred.");
+      			e.printStackTrace();
+    }
+			
+		return insert_count;
 	}
 
 	/**
